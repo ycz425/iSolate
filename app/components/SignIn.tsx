74e62197bb40dced7d7f729e1dc753682ec94a3c
John Zhang <23zhanyuc@gmail.com>
2024-09-25 11:25:46 -0400

    Add SVG icon to "Sign in with Google" button

--- app/components/SignIn.tsx
+++ app/components/SignIn.tsx
@@ -1,4 +1,5 @@
 import { signIn, auth } from "@/auth"
+import Image from "next/image"
  
 export default function SignIn() {
     return (
@@ -8,7 +9,10 @@ export default function SignIn() {
             await signIn("google")
         }}
         >
-        <button type="submit" className={"h-16 w-80 rounded-xl text-2xl bg-transparent text-black border border-black"}>Sign in with Google</button>
+            <button type="submit" className={"h-fit w-fit rounded-xl text-2xl bg-transparent text-black border border-black flex flex-row px-6 py-5 gap-5 items-center"}>
+                <Image src="/images/google-icon.svg" alt="Sign up with Google" width={40} height={40}/>
+                Sign in with Google
+            </button>
         </form>
     )
 } 
