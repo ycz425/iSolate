63134e47c1be8b7c31ca87130107b2e0287f4240
John Zhang <23zhanyuc@gmail.com>
2024-09-28 01:46:11 -0400

    Protect dashboard page with withPageAuthRequired

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,14 @@
-export default function Dashboard() {
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0"
+
+
+export default withPageAuthRequired(async function Dashboard() {
+    const { user } = await getSession()
     return (
-        <>
-            
-        </>
+        user && (
+            <div className="h-screen flex items-center justify-center border">
+                <p>Hello {user.name}!</p>
+                <a href="/api/auth/logout">Logout</a>
+            </div>
+        )
     )
-}
+}, { returnTo: "/dashboard" })
