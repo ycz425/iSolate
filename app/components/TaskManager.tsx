301dd2fa3c271c7a685c23a298025b0b41dbc243
John Zhang <23zhanyuc@gmail.com>
2024-09-30 23:45:39 -0400

    Create TaskManager.tsx

---
+++ app/components/TaskManager.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useState } from "react"
+
+export default function TaskManager() {
+    const [tabs, setTabs] = useState([])   // POPULATE DATABASE WITH SAMPLE DATA
+                                           // (but first figure out how to use Auth0 with database and how to make calls to database from next js)
+                                           // decided approach: make api endpoints here (for complex queries/operation) and make elementary database queries with adapter
+    return (
+        <>
+    
+        </>
+    )
+}
