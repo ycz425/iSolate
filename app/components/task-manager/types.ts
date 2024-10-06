8eb3296cda3b41ad923189666416243507c4c97f
John Zhang <23zhanyuc@gmail.com>
2024-10-06 10:33:49 -0400

    Define Task interface in types.ts

--- app/components/task-manager/types.ts
+++ app/components/task-manager/types.ts
@@ -2,3 +2,11 @@ export interface Tab {
     id: number,
     name: string
 }
+
+export interface Task {
+    id: number,
+    tab_id: number,
+    name: string,
+    description?: string,
+    deadline?: Date
+}
