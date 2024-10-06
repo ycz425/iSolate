44f61cfe9bc1f14189135b06f0ac0284f34aa5f3
John Zhang <23zhanyuc@gmail.com>
2024-10-06 10:38:04 -0400

    Update task-listing logic in TaskManager.tsx

--- app/components/task-manager/TaskManager.tsx
+++ app/components/task-manager/TaskManager.tsx
@@ -1,23 +1,30 @@
 "use client"
 
-import { Tab as TabInterface } from "./types"
+import { Tab as TabInterface, Task as TaskInterface } from "./types"
 import { useState } from "react"
 import Tab from "./Tab"
+import Task from "./Task"
 
 interface TaskManagerProps {
-    tabList: TabInterface[]
+    tabList: TabInterface[],
+    taskList: TaskInterface[]
 }
 
-export default function TaskManager({ tabList }: TaskManagerProps) {
-    const [tabs, setTabs] = useState(tabList)
+export default function TaskManager({ tabList, taskList }: TaskManagerProps) {
+    const [tabs, setTabs] = useState(tabList) // do we need to keep states?
     const [selectedTabId, setSelectedTabId] = useState<number | null>(null)
 
+    const [tasks, setTasks] = useState(taskList)
+
     return (
-        <div className="border w-[800px] h-96 flex flex-col items-center gap-5">
+        <div className="w-[800px] h-96 flex flex-col items-center gap-5">
             <div className="w-full h-fit flex overflow-x-scroll no-scrollbar">
                 <Tab name="Upcoming" selected={selectedTabId == null} onClick={()=>{setSelectedTabId(null)}}/>
                 {tabs.map((tab, index) => <Tab key={index} name={tab.name} selected={selectedTabId == tab.id} onClick={()=>{setSelectedTabId(tab.id)}}/>)}
             </div>
+            {tasks
+                .filter(task => selectedTabId == null || task.tab_id == selectedTabId)
+                .map((task, index) => <Task key={index} id={task.id} name={task.name}/>)}
         </div>
     )
 }
