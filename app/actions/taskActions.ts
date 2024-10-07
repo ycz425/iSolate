9a733a068627490f4f0b3eb65d77711bd227c1eb
John Zhang <23zhanyuc@gmail.com>
2024-10-07 14:51:04 -0400

    Create addTask server action

--- app/actions/taskActions.ts
+++ app/actions/taskActions.ts
@@ -2,6 +2,9 @@
 
 import { createClient } from "@supabase/supabase-js"
 import { getSession } from "@auth0/nextjs-auth0"
+import { revalidatePath } from "next/cache"
+
+// TODO: handle errors
 
 export async function getTasks(tabId?: number, tagIds?: number[]) {
     const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!) // check if service role key is needed
@@ -15,5 +18,12 @@ export async function setComplete(taskId: number) {
     const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!)
     const { user } = await getSession()
     const date = new Date()
-    const {data, error} = await supabase.from("tasks").update({completed: date.toISOString()}).eq("id", taskId)
+    await supabase.from("tasks").update({completed: date.toISOString()}).eq("user_id", user.sub).eq("id", taskId)
+}
+
+export async function addTask(name: string, description?: string, tabId?: number, deadline?: Date) {
+    const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!)
+    const { user } = await getSession()
+
+    await supabase.from("tasks").insert({user_id: user.sub, name: name, description: description, tab_id: tabId, deadline: deadline})
 }
