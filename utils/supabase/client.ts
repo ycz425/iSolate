abcf736bb688a17e4945ad40cbc36ff9bdbfafc6
John Zhang <23zhanyuc@gmail.com>
2024-10-07 14:50:18 -0400

    Create Supabase client utils

---
+++ utils/supabase/client.ts
@@ -0,0 +1,8 @@
+import { createBrowserClient } from '@supabase/ssr'
+
+export function createClient() {
+  return createBrowserClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  )
+}
