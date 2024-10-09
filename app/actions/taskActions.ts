4ab1cf03cbae1533e764e07ef27b1f9f943e606b
John Zhang <23zhanyuc@gmail.com>
2024-10-09 12:22:02 -0400

    Edit taskActions.ts

--- app/actions/taskActions.ts
+++ app/actions/taskActions.ts
@@ -3,12 +3,12 @@
 import { createClient } from "@/utils/supabase/server"
 import { getSession } from "@auth0/nextjs-auth0"
 
-// TODO: handle errors
+// TODO: handle errors and server-side form validation <3 (look at nextjs docs for server action)
 
 export async function getTasks(tabId?: number, tagIds?: number[]) {
     const supabase = createClient() // check if service role key is needed
     const { user } = await getSession()
-    const {data, error} = await supabase.from("tasks").select("id, tab_id, name, description, deadline").is("completed", null)
+    const {data, error} = await supabase.from("tasks").select("id, tab_id, name, description, deadline").eq("user_id", user.sub).is("completed", null)
 
     return data
 }
@@ -17,7 +17,7 @@ export async function setComplete(taskId: number) {
     const supabase = createClient()
     const { user } = await getSession()
     const date = new Date()
-    await supabase.from("tasks").update({completed: date.toISOString()}).eq("user_id", user.sub).eq("id", taskId)
+    await supabase.from("tasks").update({completed: date.toISOString()}).eq("id", taskId)
 }
 
 export async function addTask(name: string, description?: string, tabId?: number, deadline?: Date) {
