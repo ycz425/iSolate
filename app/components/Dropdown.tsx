f9f047e5c01cde7d7185b38c3de4a4eded1cfcb3
John Zhang <23zhanyuc@gmail.com>
2024-10-20 10:35:05 -0400

    Implement accessibility features in TaskModal

--- app/components/Dropdown.tsx
+++ app/components/Dropdown.tsx
@@ -1,11 +1,12 @@
 interface DropdownProps {
+    labelledBy: string,
     options: { name: string, value: number}[],
     onChange: (value: any) => void
 }
 
-export default function Dropdown({ options, onChange }: DropdownProps) {
+export default function Dropdown({ labelledBy, options, onChange }: DropdownProps) {
     return (
-          <select className="border-b border-neutral-300 w-1/3 text-lg outline-none focus:border-black hover:cursor-pointer" onChange={(event) => onChange(event.target.value)}>
+          <select aria-labelledby={labelledBy} className="border-b border-neutral-300 w-full text-lg outline-none focus:border-black hover:cursor-pointer" onChange={(event) => onChange(event.target.value)}>
             {options.map((option) => <option key={option.value} value={option.value}>{option.name}</option>)}
           </select>
       )
--- app/components/task-manager/TaskModal.tsx
+++ app/components/task-manager/TaskModal.tsx
@@ -38,29 +38,44 @@ export default function TaskModal({ task, tabList }: TaskModalProps) {
 
     return task && createPortal((
         <div className="absolute inset-0 bg-neutral-500 bg-opacity-50 flex justify-center items-center">
-            <div className="w-[600px] h-[400px] flex flex-col p-10 gap-5 bg-white rounded-2xl">
-                <div
-                    className="text-3xl h-10 text-nowrap overflow-hidden border-b border-neutral-300 focus:border-black outline-none"
-                    contentEditable
-                    suppressContentEditableWarning
-                    onKeyDown={onKeyDown}
-                    onInput={onNameInput}
-                >
-                    {task.name}
+            <div className="w-[600px] h-fit flex flex-col p-10 gap-5 bg-white rounded-2xl">
+                <div>
+                    <label id="name" className="text-xs text-neutral-500">Task</label>
+                    <div
+                        className="text-3xl h-10 text-nowrap overflow-hidden border-b border-neutral-300 focus:border-black outline-none"
+                        contentEditable
+                        aria-labelledby="name"
+                        suppressContentEditableWarning
+                        onKeyDown={onKeyDown}
+                        onInput={onNameInput}
+                    >
+                        {task.name} 
+                    </div>
                 </div>
                 <div className="flex gap-10">
-                    <Dropdown
-                        options={tabList.map((tab) => { return { name: tab.name, value: tab.id }})}
-                        onChange={onChange}
-                    />
-                    <input type="date" className="outline-none border-b border-neutral-300 focus:border-black"/>
+                    <div className="flex flex-col w-1/3 justify-between">
+                        <label id="category" className="text-xs text-neutral-500">Category</label>
+                        <Dropdown
+                            labelledBy="category"
+                            options={tabList.map((tab) => { return { name: tab.name, value: tab.id }})}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="flex flex-col justify-between">
+                        <label id="deadline" className="text-xs text-neutral-500">Deadline</label>
+                        <input type="datetime-local" aria-labelledby="deadline" className="outline-none border-b border-neutral-300 focus:border-black"/>
+                    </div>
                 </div>
-                <div
-                    className="w-full h-48 text-sm text-neutral-500 border border-neutral-300 p-5 rounded-xl outline-none overflow-y-scroll focus:border-black"
-                    contentEditable
-                    onInput={onDescriptionInput}
-                >
-                    {task.description}
+                <div>
+                    <label id="description" className="text-xs text-neutral-500">Description</label>
+                    <div
+                        className="w-full h-48 text-sm text-neutral-500 border border-neutral-300 p-5 rounded-xl outline-none overflow-y-scroll focus:border-black"
+                        contentEditable
+                        aria-labelledby="description"
+                        onInput={onDescriptionInput}
+                    >
+                        {task.description}
+                    </div>
                 </div>
             </div>
         </div>
