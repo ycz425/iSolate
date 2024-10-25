9194df16681be1946dba655ed642e8e06544076b
John Zhang <23zhanyuc@gmail.com>
2024-10-25 18:06:04 -0400

    Rename TagMenu to TagPopup and create new TagMenu component

--- app/components/task-manager/TagMenu.tsx
+++ app/components/task-manager/TagMenu.tsx
@@ -1,20 +1,17 @@
 import { Tag as TagInterface } from "@/app/types"
 import Tag from "./Tag"
-import { forwardRef } from 'react'
-import { ForwardedRef } from "react"
 
 interface TagMenuProps {
-    taskTags: number[],
+    selectedTags: number[],
     tagList: TagInterface[],
     onTagClick: (tagId: number) => void
 }
 
-export default forwardRef(({ taskTags, tagList, onTagClick }: TagMenuProps, ref: ForwardedRef<HTMLDivElement>) => {
+export default function TagMenu({ selectedTags, tagList, onTagClick }: TagMenuProps) {
+
     return (
-            <div ref={ref} className="border border-neutral-300 flex flex-col w-fit h-fit max-h-48 rounded-xl p-3 gap-2 shadow-md bg-white absolute">
-                <div className="flex flex-col w-fit h-fit overflow-y-scroll gap-2">
-                    {tagList.map((tag, index) => <Tag key={index} tag={tag} colored={taskTags.some(taskTag => taskTag == tag.id)} onClick={() => {onTagClick(tag.id)}}/>)}
-                </div>
-            </div>
+        <div className="flex flex-col p-3 w-full h-fit overflow-y-scroll gap-2">
+            {tagList.map((tag) => <Tag key={tag.id} tag={tag} colored={selectedTags.some(selectedTag => selectedTag == tag.id)} onClick={() => {onTagClick(tag.id)}}/>)}
+        </div>
     )
-})
+}
---
+++ app/components/task-manager/TagPopup.tsx
@@ -0,0 +1,21 @@
+import { Tag as TagInterface } from "@/app/types"
+import Tag from "./Tag"
+import { forwardRef } from 'react'
+import { ForwardedRef } from "react"
+
+interface TagPopupProps {
+    selectedTags: number[],
+    tagList: TagInterface[],
+    onTagClick: (tagId: number) => void
+}
+
+export default forwardRef(({ selectedTags, tagList, onTagClick }: TagPopupProps, ref: ForwardedRef<HTMLDivElement>) => {
+
+    return (
+            <div ref={ref} className="border border-neutral-300 flex flex-col w-fit h-fit max-h-48 rounded-xl p-3 gap-2 shadow-md bg-white absolute">
+                <div className="flex flex-col w-fit h-fit overflow-y-scroll gap-2">
+                    {tagList.map((tag) => <Tag key={tag.id} tag={tag} colored={selectedTags.some(selectedTag => selectedTag == tag.id)} onClick={() => {onTagClick(tag.id)}}/>)}
+                </div>
+            </div>
+    )
+})
