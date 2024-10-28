9052614a338bc5e6e2c118680c67fce679dcb1c7
John Zhang <23zhanyuc@gmail.com>
2024-10-28 13:12:33 -0400

    Update route.ts

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -2,6 +2,6 @@ import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
 export const GET = handleAuth({
     login: handleLogin({
-        returnTo: "http://localhost:3000/dashboard"
+        returnTo: process.env.AUTH0_LOGIN_RETURNTO
     })
 });
